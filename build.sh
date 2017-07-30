88007d91cffa7cb32dc56fc0fd841f8e581794ea
Michael Lotz <mmlr@mlotz.ch>
2017-07-30 21:51:55 +0200

    Add build script.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SOURCES=teleport.cpp
+OUTPUT=teleport
+LIBS="-lpthread"
+CPPFLAGS="-O3 -Wall -Werror -Wno-multichar -g"
+
+case $(uname) in
+	Haiku)
+		LIBS="$LIBS -lnetwork"
+	;;
+esac
+
+GCC_MAJOR_VERSION=$(gcc -v 2>&1 | tail -n1 | sed 's/[^0-9]*\([0-9]\+\).*/\1/')
+if [ $GCC_MAJOR_VERSION -gt 2 ]
+then
+	CPPFLAGS="$CPPFLAGS -Wextra -pedantic -std=c++11"
+fi
+
+g++ $SOURCES -o $OUTPUT $CPPFLAGS $LIBS
