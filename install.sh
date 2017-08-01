507ddf0c90f3a9d2fd1909bfe31b8eec5d76181b
Michael Lotz <mmlr@mlotz.ch>
2017-08-01 20:16:46 +0200

    Add install script.

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+BINARY=teleport
+
+if [ ! -e "$BINARY" ]
+then
+	echo "no binary, building"
+	./build.sh
+fi
+
+BIN_DIR=/usr/local/bin
+case $(uname) in
+	Haiku)
+		BIN_DIR="$(finddir B_USER_NONPACKAGED_BIN_DIRECTORY)"
+		echo "installing $BINARY to $BIN_DIR"
+		cp "$BINARY" "$BIN_DIR"
+	;;
+
+	*)
+		BIN_DIR=/usr/local/bin
+		echo "installing $BINARY to $BIN_DIR"
+		install --owner=root --group=root --target-directory "$BIN_DIR" -D \
+			"$BINARY"
+	;;
+esac
